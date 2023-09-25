af133b6c4808206ff2c9d8595d6f52e419ca4f4c
Snakey <130373358+snakeynotme@users.noreply.github.com>
2023-09-25 12:41:39 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-# Login ke VPS
-# (Masukkan perintah login Anda di sini)
-
-# Beralih ke akun root
-sudo su
-
-# Pindah ke direktori beranda
-cd
+# Pastikan perintah ini dijalankan sebagai root atau dengan hak akses sudo
 
 # Edit file konfigurasi SSH
-nano /etc/ssh/sshd_config
+sudo nano /etc/ssh/sshd_config
 
 # Ubah konfigurasi SSH
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 # Simpan perubahan
 # (Di dalam nano, tekan Ctrl + X, lalu tekan Y, dan tekan Enter)
 
 # Ganti kata sandi root
-passwd
+sudo passwd
 
 # Restart layanan SSH
-service ssh restart
-service sshd restart
+sudo systemctl restart ssh
 
 echo "Root login telah diaktifkan dengan kata sandi. Ingatlah untuk menjaga keamanan VPS Anda dengan baik."
